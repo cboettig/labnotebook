532032e8392fdd0f6f7546bf9a14e1b3908fab08
Carl Boettiger <cboettig@gmail.com>
2012-07-31 15:34:23 -0700

    minor comment/suggestion

--- _plugins/feedzirra/feedzirra.rb
+++ _plugins/feedzirra/feedzirra.rb
@@ -7,6 +7,7 @@ module Jekyll
   class FeedzirraBlockTag < Liquid::Tag
     def render(context)
       feed = Feedzirra::Feed.fetch_and_parse("https://github.com/cboettig.atom")
+      # consider formatting properly
       feed.title + feed.entries[0].content + feed.entries[1].content + feed.entries[2].content + feed.entries[3].content 
     end
   end
