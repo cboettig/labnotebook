cdf0c464a1048d051cc668f52fcaa0a0ccf07ed8
Carl Boettiger <cboettig@gmail.com>
2012-07-31 15:30:17 -0700

    updated octokit github API integration to pull issues/tasks list into notebook

--- _plugins/feedzirra/feedzirra.rb
+++ _plugins/feedzirra/feedzirra.rb
@@ -6,8 +6,8 @@ module Jekyll
 
   class FeedzirraBlockTag < Liquid::Tag
     def render(context)
-#      feed = Feedzirra::Feed.fetch_and_parse("https://github.com/cboettig.atom")
-#      feed.title + feed.entries[0].content + feed.entries[1].content + feed.entries[2].content + feed.entries[3].content 
+      feed = Feedzirra::Feed.fetch_and_parse("https://github.com/cboettig.atom")
+      feed.title + feed.entries[0].content + feed.entries[1].content + feed.entries[2].content + feed.entries[3].content 
     end
   end
 
--- _plugins/github_api/octokit.rb
+++ _plugins/github_api/octokit.rb
@@ -1,32 +1,39 @@
 require 'octokit'
 
+# should this cache?
 
 module Jekyll
   class OctokitIssues < Liquid::Tag
-    def render(context)
+    def initialize(tag_name, text, tokens)
+      super
+      @text = text
+      @address = "cboettig/"+"#{@text}"
+    end
 
-      repo = Octokit.issues("cboettig/pdg_control")
+
+    def render(context) # learn how to write this to take an argument!
+
+      repo = Octokit.issues(@address) # grab the data. Can this go in "initialize?"
+
+      # Generate a list of all open issues, linking to github issue page.  
+      out = "<ul>"
       for i in 0 ... repo.size
         lab = ""
-        if repo[i].labels[0].class == Hashie::Mash
-          lab = "<a class=\"btn\" style=\"background-image:none; filter:none; background-color:#" + repo[i].labels[0].color + 
-                "\">" + repo[i].labels[0].name  + "</a>"
+        if repo[i].labels[0].class == Hashie::Mash  # Get labels for issues, with color, where applicable 
+          lab = " (<font color=\"#" + repo[i].labels[0].color + 
+                "\">" + repo[i].labels[0].name  + "</font>)"
         end
-          puts "<a href=\"" + repo[i].url + "\">" +  repo[i].title + "</a>" + lab
+        if repo[i].state == "open" # Print only open issues (not necessary? Done by default?)
+          out = out + "<li> <a href=\"" + repo[i].html_url + "\">" +  repo[i].title + "</a> " + lab + "</li>"
+        end
       end
-        # consider using repo[i].labels to extract Label tag and color code information
-        # repo[i].labels[0].name 
-        # repo[i].labels[0].color
-        # repo[i].labels[0].url
-        #
-        # Remember to look up .class and methods for objects to learn how to manipulate them!  Above is much nicer than:
-        # repo[i].labels[0].to_a[0][1] #returns the color's hex value
-        # repo[i].labels[0].to_a[1][1] #returns the label's name (note the use of converting to an array)
+      out = out + "</ul>"
+      out
 
     end
   end
 end
 
-Liquid::Template.register_tag('octokit', Jekyll::OctokitIssues)
+Liquid::Template.register_tag('octokit_issues', Jekyll::OctokitIssues)
 
 
