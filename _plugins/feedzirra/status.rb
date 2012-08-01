f154c673fe15525115eb28ca1796cea36ec2a772
Carl Boettiger <cboettig@gmail.com>
2012-07-31 18:54:29 -0700

    simple script to pull recent status from twitter and github

---
+++ _plugins/feedzirra/status.rb
@@ -0,0 +1,43 @@
+require 'feedzirra'
+
+require 'twitter'
+
+# Need to store feed in cache and just call update?
+
+module Jekyll
+  class TwitterFeed < Liquid::Tag
+    def initialize(tag_name, text, tokens)
+      super
+      @user = text
+    end
+    def render(context)
+      out = "<ul class=\"unstyled\">"
+      tweets = Twitter.user_timeline(@user)
+      for i in 0 ... 3
+      out = out + "<li>" + tweets[i].text +
+        "<a href=\"http://twitter.com/" + @user + "/statuses/" + 
+        tweets[i].id.to_s + "\">"  + tweets[i].created_at.to_s + "</a> " + 
+        "</li>"
+      end
+      out + "</ul>"
+    end
+  end
+
+
+  class GithubFeed < Liquid::Tag
+    def render(context)
+      out = "<ul class=\"unstyled\">"
+      feed = Feedzirra::Feed.fetch_and_parse("https://github.com/cboettig.atom")
+      # consider formatting properly
+      for i in 0 ... 3
+        out = out + "<li>" + feed.entries[i].content + "<a href=\"" + feed.entries[i].url + "\">" +
+          feed.entries[i].published.to_s + "</a>" + "</li>" 
+      end
+      out + "</ul>"
+    end
+  end
+end
+
+Liquid::Template.register_tag('twitter_feed', Jekyll::TwitterFeed)
+Liquid::Template.register_tag('github_feed', Jekyll::GithubFeed)
+
