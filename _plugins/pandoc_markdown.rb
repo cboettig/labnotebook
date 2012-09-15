ecd9bf98b8e7c5e17b3f096c50e0840f0e68a226
Carl Boettiger <cboettig@gmail.com>
2012-09-15 00:39:58 -0700

    update appearance of site

---
+++ _plugins/pandoc_markdown.rb
@@ -0,0 +1,9 @@
+require 'pandoc-ruby'
+
+class Jekyll::MarkdownConverter
+  def convert(content)
+    return super unless ( @config['markdown'] == 'pandoc' || @config['pandoc'] )
+    @pandoc_extensions = @config['pandoc']['extensions'].map { |e| e.to_sym }
+    PandocRuby.new(content, *@pandoc_extensions).to_html
+  end
+end
