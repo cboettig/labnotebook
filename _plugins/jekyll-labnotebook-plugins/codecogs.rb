78f2650b64bceedbf61364eba8d9d120c5eb06a4
Carl Boettiger <cboettig@gmail.com>
2012-10-31 14:12:58 -0700

    codecogs plugin documentation

--- _plugins/jekyll-labnotebook-plugins/codecogs.rb
+++ _plugins/jekyll-labnotebook-plugins/codecogs.rb
@@ -1,3 +1,20 @@
+# codecogs plugin
+# Author: Carl Boettiger
+# License: MIT
+#
+# A simple filter to replace mathjax-latex syntax with images of the 
+# equations as rendered by codecogs.  This is useful to filter content
+# entered into the atom feed, where javascript and css-based rendering
+# of equations won't work.  
+
+# Credits to Noam Ross http://www.noamross.net/blog/2012/4/4/math-in-rss-feeds.html for inspiring this approach and to
+# helpful folks on SO for the regexpr: http://stackoverflow.com/questions/13166112/ruby-regexp-to-replace-equations
+
+# Example use: 
+#
+# {{ post.content | codecogs }}
+# 
+
 module TextFilter
   def codecogs(input)
   desired = input.gsub(/\\\[\s*(.*?)\s*\\\]/, "<div style=\"text-align:center;\"><img src=\"http://latex.codecogs.com/png.latex?\\1\" alt=\"\\1\" /></div>") 
