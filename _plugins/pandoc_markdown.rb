62e2d7eba4e78bc39a6c5a2a5e2eca08f2f2ac77
Carl Boettiger <cboettig@gmail.com>
2013-01-26 19:15:07 -0800

    html5 mode for pandoc doesn't close image tags (etc), resulting in non-valid XML.  Sticking with original html (XHTML1) parser

--- _plugins/pandoc_markdown.rb
+++ _plugins/pandoc_markdown.rb
@@ -4,6 +4,6 @@ class Jekyll::MarkdownConverter
   def convert(content)
     return super unless ( @config['markdown'] == 'pandoc' || @config['pandoc'] )
     @pandoc_extensions = @config['pandoc']['extensions'].map { |e| e.to_sym }
-    PandocRuby.new(content, *@pandoc_extensions).to_html5
+    PandocRuby.new(content, *@pandoc_extensions).to_html
   end
 end
