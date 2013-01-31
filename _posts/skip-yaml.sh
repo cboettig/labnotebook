32532fcc7fc241949f1e648e4796e618ab59379e
Carl Boettiger <cboettig@gmail.com>
2013-01-31 15:06:53 -0800

    fix citations

--- _posts/skip-yaml.sh
+++ _posts/skip-yaml.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 
-for f in grep -l '## References$' *.markdown *.md 
+for f in `grep -l '##* References$' *.markdown *.md`
   # *md *.markdown 
   do 
+    sed -i 's/##* References/\n## References' $f 
     sed -n '/---/,/---/p' $f > yaml.md
     sed  '1,/^$/d' $f > body.md
-    ~/.cabal/bin/./pandoc -f markdown body.md -t html -o body.html
-    ~/.cabal/bin/./pandoc body.html -t markdown_github -o out.md
+    ~/.cabal/bin/./pandoc -f markdown body.md -t html --mathjax -o body.html
+    ~/.cabal/bin/./pandoc body.html -t markdown_github --mathjax -o out.md
     cat yaml.md out.md > $f
     rm yaml.md body.md out.md body.html 
 done
