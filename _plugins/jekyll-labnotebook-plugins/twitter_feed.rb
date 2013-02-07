0cb7a11247d3dd4ca040ed156a63b6a5d2197bf7
Carl Boettiger <cboettig@gmail.com>
2013-02-06 21:49:17 -0800

    increase timeout wait from twitter API to avoid frequent errors

--- _plugins/jekyll-labnotebook-plugins/twitter_feed.rb
+++ _plugins/jekyll-labnotebook-plugins/twitter_feed.rb
@@ -23,6 +23,9 @@ module Jekyll
                                          :space_after_headers => true)
 
       out = "<ul>"
+
+
+      Twitter.connection_options = {:timeout => 50, :open_timeout => 20} # increase timeout to avoid frequent errors 
       @client = Twitter::Client.new(YAML.load_file("/home/cboettig/.twitter_auth.yaml"))
       tweets = @client.user_timeline(@user)
       for i in 0 ... @count.to_i
