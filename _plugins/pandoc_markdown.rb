8f664cc859f4977840076026e5db1019f75cb562
Carl Boettiger <cboettig@gmail.com>
2013-02-13 12:36:16 -0800

    addresses the header html problem by hardcoding a template
    not optimal, preferably a template file would only be used when the option was given in the _config.yaml
    in this solution, template file is always used.  But net effect accomplishes goal of providing data URIs when
    they are toggled on in _config.yaml and not providing them when they are toggled of in _config.yaml. Closes #58

--- _plugins/pandoc_markdown.rb
+++ _plugins/pandoc_markdown.rb
@@ -4,6 +4,8 @@ class Jekyll::MarkdownConverter
   def convert(content)
     return super unless ( @config['markdown'] == 'pandoc' || @config['pandoc'] )
     @pandoc_extensions = @config['pandoc']['extensions'].map { |e| e.to_sym }
-    PandocRuby.new(content, *@pandoc_extensions).to_html
+    ## Find pandoc extensions containing '=' and replace :option=argument with {:option => :argument}
+    @template = "assets/datauri_template.html".to_sym
+    PandocRuby.new(content, *@pandoc_extensions, {:template => @template}).to_html
   end
 end
