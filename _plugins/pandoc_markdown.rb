94fad3c0f54d5a3da48a86e8a094d596144940de
Carl Boettiger <cboettig@gmail.com>
2013-02-14 10:34:36 -0800

    minor changes ruby if doesn't need to check that variable exists, since undefined acts as false This is used in the site.sidebar_pageviews option.
    also fixed compile with correct handling of pandoc extensions

--- _plugins/pandoc_markdown.rb
+++ _plugins/pandoc_markdown.rb
@@ -1,10 +1,9 @@
 require 'pandoc-ruby'
 
 class Jekyll::MarkdownConverter
-
-  def convert(content)
-    return super unless ( @config['markdown'] == 'pandoc' || @config['pandoc'] )
-    @pandoc_extensions = @config['pandoc']['extensions']
-    PandocRuby.new(content, *@pandoc_extensions).to_html
-  end
+  def convert(content)
+    return super unless ( @config['markdown'] == 'pandoc' || @config['pandoc'] )
+    @pandoc_extensions = @config['pandoc']['extensions']
+    PandocRuby.new(content, *@pandoc_extensions).to_html
+  end
 end
