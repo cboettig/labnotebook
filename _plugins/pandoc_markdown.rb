49fefa1d1e4a4d496e334985f69f07fc5d635a54
Carl Boettiger <cboettig@gmail.com>
2013-02-14 09:46:56 -0800

    Better fix to issue #58, uses template only when asked as option code consistent with pandoc-jekyll plugin, see https://github.com/dsanson/jekyll-pandoc-plugin/pull/2

--- _plugins/pandoc_markdown.rb
+++ _plugins/pandoc_markdown.rb
@@ -2,10 +2,8 @@ require 'pandoc-ruby'
 
 class Jekyll::MarkdownConverter
   def convert(content)
-    return super unless ( @config['markdown'] == 'pandoc' || @config['pandoc'] )
-    @pandoc_extensions = @config['pandoc']['extensions'].map { |e| e.to_sym }
-    ## Find pandoc extensions containing '=' and replace :option=argument with {:option => :argument}
-    @template = "assets/datauri_template.html".to_sym
-    PandocRuby.new(content, *@pandoc_extensions, {:template => @template}).to_html
-  end
+    return super unless ( @config['markdown'] == 'pandoc' || @config['pandoc'] )
+    @pandoc_extensions = @config['pandoc']['extensions']
+    PandocRuby.new(content, *@pandoc_extensions).to_html #using to_html5 breaks xhtml
+   end
 end
