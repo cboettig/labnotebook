7ad0bb9af789fed5584dfa4214750a301afa69eb
Carl Boettiger <cboettig@gmail.com>
2013-02-14 10:02:45 -0800

    compile fails

--- _plugins/pandoc_markdown.rb
+++ _plugins/pandoc_markdown.rb
@@ -1,9 +1,10 @@
 require 'pandoc-ruby'
 
 class Jekyll::MarkdownConverter
-  def convert(content)
-    return super unless ( @config['markdown'] == 'pandoc' || @config['pandoc'] )
+
+  def convert(content)
+    return super unless ( @config['markdown'] == 'pandoc' || @config['pandoc'] )
     @pandoc_extensions = @config['pandoc']['extensions']
-    PandocRuby.new(content, *@pandoc_extensions).to_html #using to_html5 breaks xhtml
-   end
+    PandocRuby.new(content, *@pandoc_extensions).to_html
+  end
 end
