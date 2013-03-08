41796ac01f87b36ba9c3aa47855c39032a502c40
Carl Boettiger <cboettig@gmail.com>
2013-03-08 12:40:40 -0800

    config turn off old search

--- _plugins/jekyll_lunr_js_search.rb
+++ _plugins/jekyll_lunr_js_search.rb
@@ -9,52 +9,54 @@ module Jekyll
       super(config)
       
       @excludes = config['lunr_excludes'] || []
+      
+      # if web host supports index.html as default doc, then optionally exclude it from the url 
+      @strip_index_html = config['lunr_strip_index_html'] || false
     end
 
     # Index all pages except pages matching any value in config['lunr_excludes'] or with date['exclude_from_search']
     # The main content from each page is extracted and saved to disk as json
     def generate(site)
+      puts 'Running the search indexer...'
 
-      if (site.config['lunrsearchindex'])
-        puts 'Running the search indexer...'
+      # gather pages and posts
+      items = pages_to_index(site)
+      content_renderer = PageRenderer.new(site)
+      index = []
 
-        # gather pages and posts
-        items = pages_to_index(site)
-        content_renderer = PageRenderer.new(site)
-        index = []
+      items.each do |item|
+        entry = SearchEntry.create(item, content_renderer)
 
-        items.each do |item|
-          entry = SearchEntry.create(item, content_renderer)
+        entry.strip_index_suffix_from_url! if @strip_index_html
 
-          index << {
-            :title => entry.title, 
-            :url => entry.url,
-            :date => entry.date,
-            :categories => entry.categories,
-            :body => entry.body
-          }
-          
-          puts 'Indexed ' << entry.url        
-          # $stdout.print(".");$stdout.flush;
-        end
+        index << {
+          :title => entry.title, 
+          :url => entry.url,
+          :date => entry.date,
+          :categories => entry.categories,
+          :body => entry.body
+        }
         
-        json = JSON.pretty_generate({:entries => index})
-        
-        # Create destination directory if it doesn't exist yet. Otherwise, we cannot write our file there.
-        Dir::mkdir(site.dest) unless File.directory?(site.dest)
-        
-        # File I/O: create search.json file and write out pretty-printed JSON
-        filename = 'search.json'
-        
-        File.open(File.join(site.dest, filename), "w") do |file|
-          file.write(json)
-        end
-
-        # Keep the search.json file from being cleaned by Jekyll
-        site.static_files << Jekyll::SearchIndexFile.new(site, site.dest, "/", filename)
-
-        puts ''
+        puts 'Indexed ' << "#{entry.title} (#{entry.url})"
+        # $stdout.print(".");$stdout.flush;
       end
+      
+      json = JSON.pretty_generate({:entries => index})
+      
+      # Create destination directory if it doesn't exist yet. Otherwise, we cannot write our file there.
+      Dir::mkdir(site.dest) unless File.directory?(site.dest)
+      
+      # File I/O: create search.json file and write out pretty-printed JSON
+      filename = 'search.json'
+      
+      File.open(File.join(site.dest, filename), "w") do |file|
+        file.write(json)
+      end
+
+      # Keep the search.json file from being cleaned by Jekyll
+      site.static_files << Jekyll::SearchIndexFile.new(site, site.dest, "/", filename)
+
+      puts ''
     end
 
   private
@@ -63,8 +65,10 @@ module Jekyll
       items = site.pages.dup.concat(site.posts)
 
       # only process files that will be converted to .html and only non excluded files 
-      items = items.find_all {|i| i.output_ext == '.html' && ! @excludes.any? {|s| (i.url =~ Regexp.new(s)) != nil } } 
+      items.select! {|i| i.output_ext == '.html' && ! @excludes.any? {|s| (i.url =~ Regexp.new(s)) != nil } } 
       items.reject! {|i| i.data['exclude_from_search'] } 
+      
+      items
     end
   end
 end
@@ -99,12 +103,7 @@ module Jekyll
     end
     
     def self.create_from_page(page, renderer)
-      title = extract_title(page)
-      
-      url = "#{page.instance_variable_get('@dir')}"
-      url = "#{url}/" if page.index?
-      url = File.join(url, page.dir) unless page.index?
-
+      title, url = extract_title_and_url(page)
       body = renderer.render(page)
       date = nil
       categories = []
@@ -113,23 +112,27 @@ module Jekyll
     end
     
     def self.create_from_post(post, renderer)
-      title = extract_title(post)
-      url = post.url
+      title, url = extract_title_and_url(post)
       body = renderer.render(post)
       date = post.date
       categories = post.categories
       
       SearchEntry.new(title, url, date, categories, body)
     end
-    
-    def self.extract_title(item)
-      item.data['title'] || item.name
+
+    def self.extract_title_and_url(item)
+      data = item.to_liquid
+      [ data['title'], data['url'] ]
     end
 
     attr_reader :title, :url, :date, :categories, :body
     
     def initialize(title, url, date, categories, body)
       @title, @url, @date, @categories, @body = title, url, date, categories, body
+    end
+    
+    def strip_index_suffix_from_url!
+      @url.gsub!(/index\.html$/, '')
     end
   end
 
@@ -144,3 +147,5 @@ module Jekyll
   end
   
 end
+
+
