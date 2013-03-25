cd6e44231aa7aeef24f7868f44d8ee33efdb426a
Carl Boettiger <cboettig@gmail.com>
2013-03-25 15:12:45 -0700

    notes on publish script

--- publish.sh
+++ publish.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+
+## Compile the site
 jekyll 
+
+## Copy site to production server
 rsync -az _site/ carlboettiger.info:~/carlboettiger.info/
+
+## Copy site to repository for github hosting
+rsync -az _site/ ~/Documents/code/cboettig.github.com  
+
+## Need to manually commit new and changed files and push to github
