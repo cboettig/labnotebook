7c0cef1457e584a2e0cd32854700375d8641d007
Carl Boettiger <cboettig@gmail.com>
2013-04-10 14:23:55 -0700

    more explicit log message on execution

--- publish.sh
+++ publish.sh
@@ -3,9 +3,6 @@
 ## Compile the site
 jekyll 
 
-## Copy site to production server
-#rsync -az _site/ carlboettiger.info:~/carlboettiger.info/
-
 ## Copy site to repository for github hosting
 rsync -az _site/ ~/Documents/code/cboettig.github.com  
 cd ~/Documents/code/cboettig.github.com  
@@ -13,9 +10,7 @@ git add *
 git commit -a -m "update site"
 git push
 cd - 
+echo "Site updated on Github server"
 
 # echo "Please cd to ~/Documents/code/cboettig.github.com and commit and push changes"  
-
-echo "Site updated"
-
 ## Need to manually commit new and changed files and push to github
