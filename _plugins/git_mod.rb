ad87f7477c27d6b21b92a228ba3e91d5225441ee
Carl Boettiger <cboettig@gmail.com>
2013-04-12 12:22:11 -0700

    strange behavior with changes to git_modified not reflected in compiling site(??)

---
+++ _plugins/git_mod.rb
@@ -0,0 +1,28 @@
+# Title: git_mod.rb
+# Author: Carl Boettiger, @cboettig
+# Licence: MIT
+# Description: A custom filter to return the date that git has for the page modification time 
+# 
+# REQUIREMENTS: Uses `path` variable set in the base_name.rb plugin
+#
+# Example use: 
+#
+# {{ post.path | prepend:'../_posts/' | git_modified }}
+# 
+
+require 'chronic'
+
+module TextFilter
+  def git_mod(input)
+    path = input 
+    modif = `git log -n 1 --format="%ai" -- #{path}`
+    modif = Chronic.parse(modif)
+    if modif.class != Time
+      puts "Error in obtaining time for"
+      puts path
+      puts modif
+    end
+    modif
+  end
+end
+Liquid::Template.register_filter(TextFilter)
