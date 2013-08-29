71ca85af98b625e12b962cbd0f3c784e04daba45
Carl Boettiger <cboettig@gmail.com>
2013-08-29 12:21:54 -0700

    added adaptive dynamics notes to repository

--- publish.sh
+++ publish.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-## Compile the site
-jekyll build 
 
 ## Make sure github hosting repo is up-to-date before we write to it
 cd ~/Documents/code/cboettig.github.com  
-git pull
+git pull -s ours
 cd - 
 
-## Copy site to repository for github hosting
-rsync -az _site/ ~/Documents/code/cboettig.github.com  
+## Compile the site
+jekyll build -d ~/Documents/code/cboettig.github.com  ## Preferable to rsync?
+
+
+## Upload the site to github 
+##rsync -az _site/ ~/Documents/code/cboettig.github.com  
 cd ~/Documents/code/cboettig.github.com  
 git add *
 git commit -a -m "update site"
-git pull -s ours
 git push
 echo "Site updated on Github server"
 
-# echo "Please cd to ~/Documents/code/cboettig.github.com and commit and push changes"  
-## Need to manually commit new and changed files and push to github
