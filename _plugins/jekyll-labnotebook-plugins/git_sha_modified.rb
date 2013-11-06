b5598fd3a3803631f09fe6b91f63883a708dc292
Carl Boettiger <cboettig@gmail.com>
2013-11-06 16:47:25 -0500

    added external data file to possibly speed up sha/mod calculations

--- _plugins/jekyll-labnotebook-plugins/git_sha_modified.rb
+++ _plugins/jekyll-labnotebook-plugins/git_sha_modified.rb
@@ -10,11 +10,22 @@
 # 
 
 require 'git'
+require 'json'
+
 module Jekyll
   class RawContent < Generator
-    def generate(site)
 
-      
+
+    def generate(site)
+  
+      def niltext(char)
+        if(char.nil?)
+          "not computed"
+        else
+          char
+        end
+      end
+     
       puts "Getting date modified and sha data from git"
       start = Time.now
       g = Git.open("/home/cboettig/Documents/labnotebook")
@@ -24,21 +35,42 @@ module Jekyll
       # generate a data file and read in as a hash containing the path, sha, and commit time
       # for each post.  Then in looping over posts we could just look up sha and timestamp
       # from the path in the hash, rather than calling git gem each time.  
+      if(!File.exists?("gitdata.json"))
 
-      ## Loop over posts 
-      site.posts.each do |post|
-        begin
-          path = '_posts/' + post.name
-          modified = g.log(1).object(path).first.date
-          sha = g.log(1).object(path).first.sha
-        rescue Exception => e 
-          modified = Time.now 
-          sha = "not yet committed" 
+        ## Loop over posts 
+        site.posts.each do |post|
+          begin
+            path = '_posts/' + post.name
+        
+            modified = g.log(1).object(path).first.date
+            sha = g.log(1).object(path).first.sha
+          rescue Exception => e 
+            modified = Time.now 
+            sha = "not yet committed" 
+          end
+          post.data['sha'] = sha
+          post.data['modified'] = modified
         end
-        post.data['sha'] = sha
-        post.data['modified'] = modified
+      
+        gitdata = Hash[site.posts.collect{|post| 
+          [post.path, [post.data['sha'], post.data['modified']]]}]
+
+        File.open("gitdata.json", "w") do |f|
+              f.write(JSON.pretty_generate(gitdata))
+        end
+      end 
+
+      if(File.exists?("gitdata.json"))
+        buffer = open("gitdata.json")
+        gitdata = JSON.load(buffer)
+        site.posts.each do |post|
+            path = '_posts/' + post.name
+            post.data['sha'] =  niltext(gitdata[path][0])
+            post.data['modified'] =  niltext(gitdata[path][1])
+        end
       end
-      ## Loop over pages 
+
+  ## Loop over pages 
       site.pages.each do |page|
         begin
           path = File.join(".", page.dir, page.name)
@@ -54,7 +86,6 @@ module Jekyll
 
       finish = Time.now
       puts "Time elapsed #{(finish - start)} seconds"
-
 
     end
   end
