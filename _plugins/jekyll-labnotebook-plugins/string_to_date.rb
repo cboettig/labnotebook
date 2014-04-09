f339fbac5c196c0fc1c44a4b35c3c617a0ab7449
Carl Boettiger <cboettig@gmail.com>
2014-04-09 13:22:59 -0700

    update site

---
+++ _plugins/jekyll-labnotebook-plugins/string_to_date.rb
@@ -0,0 +1,21 @@
+# Title: codecogs plugin
+# Author: Carl Boettiger, @cboettig
+# License: MIT
+#
+# A simple filter to convert strings to dates
+# Example use:
+#
+# {{ post.modified | string_to_date }}
+#
+
+require 'chronic'
+
+module Jekyll
+  module StringToDateFilter
+    def codecogs(input)
+      puts input
+     input
+    end
+  end
+end
+Liquid::Template.register_filter(Jekyll::StringToDateFilter)
