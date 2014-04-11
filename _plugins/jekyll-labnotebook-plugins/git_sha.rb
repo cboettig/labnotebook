c78b3627078abbc9c4daf18da199d51c99c9e82e
Carl Boettiger <cboettig@gmail.com>
2014-04-11 14:04:41 -0700

    vita should still get sha

---
+++ _plugins/jekyll-labnotebook-plugins/git_sha.rb
@@ -0,0 +1,23 @@
+# Author: Carl Boettiger, @cboettig
+# License: MIT
+#
+# A simple filter to access the sha of a page
+# Example use:
+#
+# {{ page.name | string_to_date }}
+#
+
+require 'git'
+require 'json'
+
+module Jekyll
+  module GitSHAFilter
+    def git_sha(input)
+      path = inpu # File.join(".", page.dir, page.name)
+      # modified = g.log(1).object(path).first.date
+      sha = g.log(1).object(path).first.sha
+      sha
+    end
+  end
+end
+Liquid::Template.register_filter(Jekyll::GitSHAFilter)
