e7bf22e6197ef809c35e66b3b8e5b6957f0eed8d
Carl Boettiger <cboettig@gmail.com>
2014-04-11 17:20:33 -0700

    octokit doesnt need auth

--- _plugins/jekyll-labnotebook-plugins/octokit.rb
+++ _plugins/jekyll-labnotebook-plugins/octokit.rb
@@ -12,10 +12,9 @@
 
 
 
-# require 'octokit'
-# require 'time'
-# require 'chronic'
-# require 'yaml'
+require 'octokit'
+require 'time'
+require 'chronic'
 
 module Jekyll
   class OctokitIssues < Liquid::Tag
@@ -25,30 +24,29 @@ module Jekyll
       @address = "cboettig/"+"#{@text}"
     end
     def render(context) # learn how to write this to take an argument!
-#       puts "Getting Github Issues via octokit.rb"
-#
-#       cred = YAML.load_file("/home/cboettig/.github_cred.yml")
-#       client = Octokit::Client.new(:login => cred[":username"], :password => cred[":password"])
-#       repo = client.issues(@address) # grab the data. Can this go in "initialize?"
-#       #  repo = Octokit.issues(@address, :status => "closed") # (Gets closed issues??)
-#       # Generate a list of all open issues, linking to github issue page.
-#       out = "<ul>"
-#       for i in 0 ... [repo.size, 8].min ## displays up to 5.  sorted by date?
-#         lab = ""
-#         if repo[i].labels[0].class == Hashie::Mash  # Get labels for issues, with color, where applicable
-#           lab = " (<font color=\"#" + repo[i].labels[0].color +
-#                 "\">" + repo[i].labels[0].name  + "</font>)"
-#         end
-#         ## Actually only pulls open issues
-#         if repo[i].state == "open" # Print only open issues
-#           out = out + "<li> <a href=\"" + repo[i].html_url + "\">" +  repo[i].title + "</a> " + lab + "</li>"
-#         end
-#         if repo[i].state == "closed" # strike out closed issues
-#           out = out + "<li> <strike> <a href=\"" + repo[i].html_url + "\">" +  repo[i].title + "</a> " + lab + "</strike> </li>"
-#         end
-#       end
-#       out = out + "</ul>"
-#       out
+       puts "Getting Github Issues via octokit.rb"
+
+
+       repo = Octokit.issues(@address) # grab the data. Can this go in "initialize?"
+       #  repo = Octokit.issues(@address, :status => "closed") # (Gets closed issues??)
+       # Generate a list of all open issues, linking to github issue page.
+       out = "<ul>"
+       for i in 0 ... [repo.size, 8].min ## displays up to 5.  sorted by date?
+         lab = ""
+         if repo[i].labels[0].class == Hashie::Mash  # Get labels for issues, with color, where applicable
+           lab = " (<font color=\"#" + repo[i].labels[0].color +
+                 "\">" + repo[i].labels[0].name  + "</font>)"
+         end
+         ## Actually only pulls open issues
+         if repo[i].state == "open" # Print only open issues
+           out = out + "<li> <a href=\"" + repo[i].html_url + "\">" +  repo[i].title + "</a> " + lab + "</li>"
+         end
+         if repo[i].state == "closed" # strike out closed issues
+           out = out + "<li> <strike> <a href=\"" + repo[i].html_url + "\">" +  repo[i].title + "</a> " + lab + "</strike> </li>"
+         end
+       end
+       out = out + "</ul>"
+       out
     end
   end
 end
@@ -68,28 +66,26 @@ module Jekyll
       @address = "cboettig/"+"#{@text}"
     end
     def render(context)
-#       puts "Getting Github Commits via octokit.rb"
-#       # day = Time.now # context.environments.first["page"]["date"]
-#       # @until = Chronic.parse("Now") #(day + 60*60*24).iso8601
-#       # @since = Chronic.parse("One day ago") #day.iso8601
-#       # repo = Octokit.commits(@address, "master", {:since => @since, :until => @until})
-#       cred = YAML.load_file("/home/cboettig/.github_cred.yml")
-#       client = Octokit::Client.new(:login => cred[":username"], :password => cred[":password"])
-#       repo = client.commits(@address, "master")
-#       out = "<ul>"
-#       for i in 0 ... [repo.size, 8].min
-#         out = out + "<li>" +
-#           repo[i].commit.message + " " +
-#           "<a href=\"" +
-#           ## Adjust the link to a proper url
-#           repo[i].commit.url.gsub("api\.", "").gsub("repos/","").gsub("git/", "").gsub("commits/", "commit/") +
-#           "\">" +
-#           DateTime.parse(repo[i].commit.author.date).to_time.strftime("%I:%M %P %Y/%m/%d") +
-#           "</a>" +
-#           "</li>"
-#       end
-#       out = out + "</ul>"
-#       out
+       puts "Getting Github Commits via octokit.rb"
+       # day = Time.now # context.environments.first["page"]["date"]
+       # @until = Chronic.parse("Now") #(day + 60*60*24).iso8601
+       # @since = Chronic.parse("One day ago") #day.iso8601
+       # repo = Octokit.commits(@address, "master", {:since => @since, :until => @until})
+       repo = Octokit.commits(@address, "master")
+       out = "<ul>"
+       for i in 0 ... [repo.size, 8].min
+         out = out + "<li>" +
+           repo[i].commit.message + " " +
+           "<a href=\"" +
+           ## Adjust the link to a proper url
+           repo[i].commit.url.gsub("api\.", "").gsub("repos/","").gsub("git/", "").gsub("commits/", "commit/") +
+           "\">" +
+           DateTime.parse(repo[i].commit.author.date).to_time.strftime("%I:%M %P %Y/%m/%d") +
+           "</a>" +
+           "</li>"
+       end
+       out = out + "</ul>"
+       out
     end
   end
 end
