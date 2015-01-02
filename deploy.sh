482a09c3748a9f2fa160e2a499cce8128fc76601
Carl Boettiger <cboettig@gmail.com>
2015-01-02 15:06:06 -0500

    deploy from script instead of make

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+
+source /home/cboettig/.notebook-env.sh
+
+docker run \
+	-v $(PWD):/data \
+	-w /data --rm \
+	-e TWIT_KEY -e TWIT_SECRET -e TWIT_TOK \
+	-e TWIT_TOK_SECRET -e GIT_NAME -e GIT_EMAIL \
+	-e GH_TOKEN -e GARB_KEY -e GARB_PASS -e GARB_UA \
+	cboettig/labnotebook
+
 if [ ! -d _gh-pages ]; then
 	git clone -b master git@github.com:cboettig/cboettig.github.io _gh-pages
-# Control will enter here if $DIRECTORY exists.
 else
 	cd _gh-pages && git pull origin master && cd ..
 fi
