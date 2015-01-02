79f77a524db28ed1402174ea2922d945be78d2a6
Carl Boettiger <cboettig@gmail.com>
2015-01-02 16:49:36 -0500

    tweak deploy for cron

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
+set -e
 source /home/cboettig/.notebook-env.sh
-
-docker run \
-	-v $(PWD):/data \
-	-w /data --rm \
-	-e TWIT_KEY -e TWIT_SECRET -e TWIT_TOK \
-	-e TWIT_TOK_SECRET -e GIT_NAME -e GIT_EMAIL \
-	-e GH_TOKEN -e GARB_KEY -e GARB_PASS -e GARB_UA \
-	cboettig/labnotebook
+docker run --rm -v $(pwd):/data -w /data -e TWIT_KEY -e TWIT_SECRET -e TWIT_TOK -e TWIT_TOK_SECRET -e GARB_KEY -e GARB_PASS -e GARB_UA cboettig/labnotebook
 
 if [ ! -d _gh-pages ]; then
 	git clone -b master git@github.com:cboettig/cboettig.github.io _gh-pages
