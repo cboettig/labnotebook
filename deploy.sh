e87f8e75096d68529654e394fa07a93a7bb05e30
Carl Boettiger <cboettig@gmail.com>
2015-01-04 01:39:06 -0500

    working deploy on drone from push

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -i
-set -e
 
 if [ "$DRONE_BRANCH" ]
   then 
@@ -8,12 +7,12 @@ if [ "$DRONE_BRANCH" ]
         sudo apt-get update && sudo apt-get install -y rsync
         git config --global user.name cboettg
         git config --global user.email cboettig@server.com
+	git config --global push.default simple
         git clone -b source https://github.com/cboettig/cboettig.github.io ../source
         cd ../source && Rscript -e 'servr::jekyll(serve=FALSE)'
         cd .. && git clone -b master https://cboettig:${GH_TOKEN}@github.com/cboettig/cboettig.github.io deploy 
         rsync -av source/_site/ deploy/
         cd deploy
-        git status
-        git add -A . && git commit -m 'Site updated' && git push
+        git add -A . && git commit -m 'Site updated from drone' && git push
     fi
 fi
