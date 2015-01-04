506443997300895b5ce368987ebdc86dd6ba1516
Carl Boettiger <cboettig@gmail.com>
2015-01-03 23:59:44 -0500

    drone deploy test

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-
 set -e
-source /home/cboettig/.notebook-env.sh
-docker run --rm -v $(pwd):/data -w /data -e TWIT_KEY -e TWIT_SECRET -e TWIT_TOK -e TWIT_TOK_SECRET -e GARB_KEY -e GARB_PASS -e GARB_UA cboettig/labnotebook
+git config user.name cboettig
+git config user.email cboettig@server.com
 
 if [ ! -d _gh-pages ]; then
-	git clone -b master git@github.com:cboettig/cboettig.github.io _gh-pages
+	git clone -b master https://cboettig:${GH_TOKEN}@github.com:cboettig/cboettig.github.io _gh-pages
 else
 	cd _gh-pages && git pull origin master && cd ..
 fi
 rsync -a _site/ _gh-pages/
 cd _gh-pages && git add -A . && git commit -m 'Site updated' && git push
-
