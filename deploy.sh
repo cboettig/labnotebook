bc586c4a7fcf78bfd5aa0433d0b97b554787dca3
Carl Boettiger <cboettig@gmail.com>
2015-01-04 01:30:57 -0500

    debugging

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,12 @@ if [ "$DRONE_BRANCH" ]
         git config --global user.email cboettig@server.com
         git clone -b source https://github.com/cboettig/cboettig.github.io ../source
         cd ../source && Rscript -e 'servr::jekyll(serve=FALSE)'
-        cd .. && git clone https://cboettig:${GH_TOKEN}@github.com/cboettig/cboettig.github.io deploy 
-        cp -a source/_site/ deploy/ 
-        cd deploy && git add -A . && git commit -m 'Site updated' && git push
+        cd .. && git clone -b master https://cboettig:${GH_TOKEN}@github.com/cboettig/cboettig.github.io deploy 
+        cp -a source/_site/ deploy/
+        ls 
+        cd deploy
+        git status
+        ls
+        git add -A . && git commit -m 'Site updated' && git push
     fi
 fi
