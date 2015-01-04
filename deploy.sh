e6ee8fa6164006aa5f8d48cd92e96a3ae55f50c2
Carl Boettiger <cboettig@gmail.com>
2015-01-04 00:31:35 -0500

    simplify deploy

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+
 set -e
-git config user.name cboettig
-git config user.email cboettig@server.com
+source /home/cboettig/.notebook-env.sh
+docker run --rm -v $(pwd):/data -w /data -e TWIT_KEY -e TWIT_SECRET -e TWIT_TOK -e TWIT_TOK_SECRET cboettig/labnotebook
 
 if [ ! -d _gh-pages ]; then
-	git clone -b master https://cboettig:${GH_TOKEN}@github.com:cboettig/cboettig.github.io _gh-pages
+	git clone -b master git@github.com:cboettig/cboettig.github.io _gh-pages
 else
 	cd _gh-pages && git pull origin master && cd ..
 fi
 rsync -a _site/ _gh-pages/
 cd _gh-pages && git add -A . && git commit -m 'Site updated' && git push
+
