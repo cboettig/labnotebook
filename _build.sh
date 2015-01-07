03c9522c46ebb3d583fcae1593af12a155406ae6
Carl Boettiger <cboettig@gmail.com>
2015-01-07 01:22:09 -0500

    fix build script

--- _build.sh
+++ _build.sh
@@ -5,6 +5,6 @@ if [ "$DRONE_BRANCH" ]
   then 
     if [ "$DRONE_BRANCH" = "$SOURCE" ]
       then 
-				Rscript -e 'servr::jekyll(serve=FALSE)'
+        Rscript -e 'servr::jekyll(serve=FALSE, script="_build.R")'
     fi
 fi
