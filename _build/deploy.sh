9aea94c759f680c5a297027437652c528f2e3824
Carl Boettiger <cboettig@gmail.com>
2015-01-08 14:55:27 -0500

    deploy quitely

--- _build/deploy.sh
+++ _build/deploy.sh
@@ -9,9 +9,9 @@ if [ "$DRONE_BRANCH" ]
   then 
     if [ "$DRONE_BRANCH" = "$SOURCE" ]
       then
-				## Deploy site: clone the gh-pages branch, rsync files, commit, and push
+	## Deploy site: clone the gh-pages branch, rsync files, commit, and push
         cd .. && git clone -b ${DEPLOY} https://cboettig:${GH_TOKEN}@github.com/cboettig/$REPO deploy 
-        rsync -av $REPO/_site/ deploy/
+        rsync -a $REPO/_site/ deploy/
         cd deploy && git add -A . && git commit -m 'Site updated from drone' && git push
     fi
 fi
