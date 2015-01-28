6daa4b1d530504f68817fc5f4132379243014ff8
Carl Boettiger <cboettig@gmail.com>
2015-01-28 00:34:18 -0500

    correct auto-commit message

--- _build/cron.sh
+++ _build/cron.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-cd /home/cboettig/lab-notebook/cboettig.github.io
+
+
+# env variables
+REPO="cboettig.github.io"
+DEPLOY="master"
 source /home/cboettig/.notebook-env.sh
+
+## Set working directory
+cd /home/cboettig/lab-notebook/cboettig.github.io
+
+## Build site
 docker run --rm -ti -v $(pwd):/data \
-  -e TWIT_KEY=TWIT_KEY -e  TWIT_SECRET=TWIT_SECRET -e TWIT_TOK=TWIT_TOK -e TWIT_TOK_SECRET=TWIT_TOK_SECRET \
+  -e TWIT_KEY=$TWIT_KEY -e TWIT_SECRET=$TWIT_SECRET -e TWIT_TOK=$TWIT_TOK -e TWIT_TOK_SECRET=$TWIT_TOK_SECRET \
   cboettig/labnotebook Rscript -e 'servr::jekyll(serve=FALSE, script="_build/build.R")'
-rsync -a _site/ _gh-pages/
-cd _gh-pages && git add -A . && git commit -m 'cron update'
+
+## Deploy site
+  cd .. && git clone -b ${DEPLOY} https://cboettig:${GH_TOKEN}@github.com/cboettig/$REPO deploy 
+rsync -a $REPO/_site/ deploy/
+  cd deploy && git add -A . && git commit -m 'Site updated from cron' && git push
+
