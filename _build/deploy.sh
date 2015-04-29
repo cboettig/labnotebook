dd670e237aadc44b1b66d9c33daf53d62311e3fe
Carl Boettiger <cboettig@gmail.com>
2015-04-29 13:31:36 -0700

    update for circle

--- _build/deploy.sh
+++ _build/deploy.sh
@@ -6,19 +6,19 @@ DEPLOY="master"
 YEAR="2015"
 
 ## Only build the site if the commit hook was a push to the source branch.
-if [ "$DRONE_BRANCH" ]
-  then 
-    if [ "$DRONE_BRANCH" = "$SOURCE" ]
-      then
+#if [ "$DRONE_BRANCH" ]
+#  then 
+#    if [ "$DRONE_BRANCH" = "$SOURCE" ]
+#      then
 	## Deploy site: clone the gh-pages branch, rsync files, commit, and push
-        cd .. && git clone -b ${DEPLOY} https://cboettig:${GH_TOKEN}@github.com/cboettig/$REPO deploy 
-        rsync -a $REPO/_site/ deploy/
-        cd deploy \
+        git clone -b ${DEPLOY} https://cboettig:${GH_TOKEN}@github.com/cboettig/$REPO ../deploy 
+        rsync -a _site/ ../deploy/
+        cd ../deploy \
             && wget http://carlboettiger.info/${YEAR}/blog.xml \
             && wget http://carlboettiger.info/${YEAR}/atom.xml \
             && wget http://carlboettiger.info/${YEAR}/R.xml \
             && git add -A . \
             && git commit -m 'Site updated from drone' \
             && git push
-    fi
-fi
+#    fi
+#fi
