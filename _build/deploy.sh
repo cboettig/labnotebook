8eac5a3865e5e90394f94de61e673960db158c7c
Carl Boettiger <cboettig@gmail.com>
2015-04-30 14:21:30 -0700

    edit commit msg

--- _build/deploy.sh
+++ _build/deploy.sh
@@ -5,20 +5,14 @@ SOURCE="source"
 DEPLOY="master"
 YEAR="2015"
 
-## Only build the site if the commit hook was a push to the source branch.
-#if [ "$DRONE_BRANCH" ]
-#  then 
-#    if [ "$DRONE_BRANCH" = "$SOURCE" ]
-#      then
-	## Deploy site: clone the gh-pages branch, rsync files, commit, and push
-        git clone -b ${DEPLOY} https://cboettig:${GH_TOKEN}@github.com/cboettig/$REPO ../deploy 
-        rsync -a _site/ ../deploy/
-        cd ../deploy \
-            && wget http://carlboettiger.info/${YEAR}/blog.xml \
-            && wget http://carlboettiger.info/${YEAR}/atom.xml \
-            && wget http://carlboettiger.info/${YEAR}/R.xml \
-            && git add -A . \
-            && git commit -m 'Site updated from drone' \
-            && git push
-#    fi
-#fi
+git clone -b ${DEPLOY} https://cboettig:${GH_TOKEN}@github.com/cboettig/$REPO ../deploy 
+rsync -a _site/ ../deploy/
+cd ../deploy \
+    && wget http://carlboettiger.info/${YEAR}/blog.xml \
+    && wget http://carlboettiger.info/${YEAR}/atom.xml \
+    && wget http://carlboettiger.info/${YEAR}/R.xml \
+    && git add -A . \
+    && git commit -m 'Site updated from circleci' \
+    && git push
+
+
