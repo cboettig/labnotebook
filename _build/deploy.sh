426f703c4ba4a2f35e4988c154ca68ee2a147e46
Carl Boettiger <cboettig@gmail.com>
2015-06-16 12:13:18 -0700

    wget must overwrite for xml atom feeds to update

--- _build/deploy.sh
+++ _build/deploy.sh
@@ -8,9 +8,9 @@ YEAR="2015"
 git clone -b ${DEPLOY} https://cboettig:${GH_TOKEN}@github.com/cboettig/$REPO ../deploy 
 rsync -a _site/ ../deploy/
 cd ../deploy \
-    && wget http://carlboettiger.info/${YEAR}/blog.xml \
-    && wget http://carlboettiger.info/${YEAR}/atom.xml \
-    && wget http://carlboettiger.info/${YEAR}/R.xml \
+    && wget -q http://carlboettiger.info/${YEAR}/blog.xml -O blog.xml \
+    && wget -q http://carlboettiger.info/${YEAR}/atom.xml -O atom.xml \
+    && wget -q http://carlboettiger.info/${YEAR}/R.xml -O R.xml \
     && git add -A . \
     && git commit -m 'Site updated from circleci' \
     && git push
