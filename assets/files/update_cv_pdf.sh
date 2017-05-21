89d3902a76b54b60f1339d5e3e97763b8ae743a5
Carl Boettiger <cboettig@gmail.com>
2017-05-20 22:24:07 -0700

    update pdf cv

--- assets/files/update_cv_pdf.sh
+++ assets/files/update_cv_pdf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd assets/files
-cp ../../vita.md .
+#cd assets/files
+cp ../../content/vita.md .
 #pandoc vita.md --template cv-template.tex -f markdown-startnum -o cv.tex
 pandoc vita.md --template cv-template.tex -f markdown-startnum -o cv.pdf
 rm vita.md
